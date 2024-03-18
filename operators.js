70d4a28917a64a2200fd25222d5fd894e5ff7cba
Esther-Naserian <naseriansamuria@gmail.com>
2024-03-18 22:26:48 +0300

    operation.js

---
+++ operators.js
@@ -0,0 +1,56 @@
+let a = 15;
+let b = 4;
+let c = 15%4;
+console.log(c);
+
+let looselyEqualTo=a==b;
+console.log({looselyEqualTo});
+
+let strictlyEqualTo=a==b;
+console.log({strictlyEqualTo});
+
+let notEqualTo=a!=b;
+console.log({notEqualTo});
+
+let d = "Gitahi is";
+let e = "a student at AkiraChix";
+let f = d+ " " +e;
+console.log(f);
+
+//example
+ d= "Gitahi is"
+e = "a student at Akirachix"
+f= `${d} ${e}`;
+console.log(f);
+
+
+//example 3
+let r = "Good Morning"
+console.log(r.concat(',','Hello World'));
+
+//example4
+let facebook ="facebook"
+let instagram = "instagram"
+let text = 'I am A vailable on' + facebook + 'and' + instagram;
+console.log(text);
+
+
+let s = 15;
+let n = 4;
+let t = 15/4
+console.log({t});
+console.log('t' , typeof t)
+
+let y = 12;
+console.log(typeof y);
+
+let j;
+console.log(j);
+console.log(typeof j)
+
+let m = null;
+console.log(m);
+console.log(typeof m )
+
+
+
